0cb6fbdfa44383b4922b845db1a2d72063c75e9a
shb18911 <146206937+shb18911@users.noreply.github.com>
2023-10-15 18:40:31 +0530

    Update Disk-Usage.sh

--- Disk-Usage.sh
+++ Disk-Usage.sh
@@ -3,5 +3,5 @@ mem=`df -h . | awk -F " " 'NR==2 {print $(NF-1)}' | sed 's/%//g'`
 
 if [ $mem -ge 30 ] ; then
         # echo "This is the email body." | mail -s "Subject of the email" bharathsh98@gmail.com
-          echo "Disk Usage Alert !!!" | mail "Disk usage reached threshould value" bharathsh98@gmail.com
+          echo "Disk Usage Alert !!!" | mail -s "Disk usage reached threshould value" bharathsh98@gmail.com
 fi
